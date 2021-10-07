70a5110a240e7ce87a73fee9e299a6a324e9e91e
kcinnoy <yonnick00@gmail.com>
2021-10-07 20:09:38 +0100

    updated navbar and added css

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/eventsDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar';
 
 function App() {
   return (
-    <div >
+    <>
       <NavBar/>
-      <Button icon='user' content='React Button' color='green' />
-      <EventDashboard/>
-    </div>
+        <Container className='main'>
+            <EventDashboard/>
+        </Container>
+    </>
   );
 }
 
