ba9c17911070e3c386b511d2adbdd6dbf8c9d1c0
kcinnoy <yonnick00@gmail.com>
2021-10-09 23:35:46 +0100

    mimic sign/sign out functionality

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/eventsDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import EventDetailPage from '../../features/eventDetailed/EventDetailPage';
 import EventForm from '../../features/events/eventForm/EventForm';
-
 
 function App() {
     const [formOpen, setFormOpen] = useState(false);
@@ -24,13 +23,20 @@ function App() {
 
     return (
         <>
-            <NavBar setFormOpen={handleCreateFormOpen} />
-            <Container className='main'>
-                <Route exact path='/' component={HomePage} />
-                <Route exact path='/events' component={EventDashboard} />
-                <Route path='/events/:id' component={EventDetailPage} />
-                <Route path='/createEvent' component={EventForm} />
-            </Container>
+            <Route exact path='/' component={HomePage} />
+            <Route
+                path={'/(.+)'}
+                render={() => (
+                    <>
+                        <NavBar setFormOpen={handleCreateFormOpen} />
+                        <Container className='main'>
+                            <Route exact path='/events' component={EventDashboard} />
+                            <Route path='/events/:id' component={EventDetailPage} />
+                            <Route path='/createEvent' component={EventForm} />
+                        </Container>
+                    </>
+                )}
+            />
         </>
     );
 }
--- revents/src/features/nav/NavBar.jsx
+++ revents/src/features/nav/NavBar.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
+import SignedOutMenu from './SignedOutMenu';
+import SignedInMenu from './SignedInMenu';
 
+export default function NavBar({ setFormOpen }) {
 
-export default function NavBar({setFormOpen}) {
+    const history = useHistory();
+    const [authenticated, setAuthenticated] = useState(false);
+
+    function handleSignOut(){
+        setAuthenticated(false);
+        history.push('/');
+    }
+
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -11,14 +21,23 @@ export default function NavBar({setFormOpen}) {
                     <img src='/assets/logo.png' alt='logo' style={{ marginRight: 15 }} />
                     Re-vents
                 </Menu.Item>
-                <Menu.Item name='Events' as={NavLink} to='/events'  />
-                <Menu.Item as={NavLink} to='/createEvent'>
-                    <Button positive inverted content onClick={() => setFormOpen(true)} content='Create Events' />
-                </Menu.Item>
-                <Menu.Item position='right'>
-                    <Button basic inverted content='login' />
-                    <Button basic inverted content='Register' style={{ marginLeft: '0.5em' }} />
-                </Menu.Item>
+                <Menu.Item name='Events' as={NavLink} to='/events' />
+                {authenticated && (
+                    <Menu.Item as={NavLink} to='/createEvent'>
+                        <Button
+                            positive
+                            inverted
+                            content
+                            onClick={() => setFormOpen(true)}
+                            content='Create Events'
+                        />
+                    </Menu.Item>
+                )}
+                {authenticated ? (
+                    <SignedInMenu signOut={handleSignOut} />
+                ) : (
+                    <SignedOutMenu setAuthenticated={setAuthenticated} />
+                )}
             </Container>
         </Menu>
     );
