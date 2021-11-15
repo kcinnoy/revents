e32ce832e00aae867011108cc449ef2303a4b295
kcinnoy <yonnick00@gmail.com>
2021-11-15 22:27:57 +0000

    added test load async button action

--- revents/src/features/sandbox/Sandbox.jsx
+++ revents/src/features/sandbox/Sandbox.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'semantic-ui-react';
 import { increment, decrement } from './testReducer';
@@ -7,37 +7,56 @@ import TestPlaceInput from './TestPlaceInput';
 import TestMap from './TestMap';
 
 export default function Sandbox() {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch(null);
+    const [target,setTarget] = useState();
     const data = useSelector(state => state.test.data);
+    const { loading } = useSelector(state => state.async);
     const defaultProps = {
         center: {
             lat: 10.99835602,
             lng: 77.01502627
         },
-        zoom: 11,
+        zoom: 11
     };
 
     const [location, setLocation] = useState(defaultProps);
 
     function handleSetLocation(latLng) {
-        setLocation({...location, center: {lat: latLng.lat, lng: latLng.lng }})
+        setLocation({ ...location, center: { lat: latLng.lat, lng: latLng.lng } });
     }
 
     return (
         <>
             <h1>test header</h1>
             <h3>data is: {data} </h3>
-            <Button onClick={() => dispatch(increment(20))} content='Increment' color='green' />
-            <Button onClick={() => dispatch(decrement(10))} content='Decrement' color='red' />
             <Button
-                onClick={() => dispatch(openModal({ modalType: 'TestModal', modalProps: {data} }))}
+                loading={loading && target === 'increment'}
+                onClick={(e) => {
+                    dispatch(increment(20));
+                    setTarget(e.target.name)
+                }}
+                content='Increment'
+                color='green'
+            />
+            <Button
+                loading={loading && target === 'decrement'}
+                onClick={(e) => {
+                    dispatch(decrement(10));
+                    setTarget(e.target.name)
+                }}
+                content='Decrement'
+                color='red'
+            />
+            <Button
+                onClick={() =>
+                    dispatch(openModal({ modalType: 'TestModal', modalProps: { data } }))
+                }
                 content='Open model'
                 color='teal'
             />
-            <div style={{marginTop: 15}}>
-                <TestPlaceInput setLocation={handleSetLocation}/>
+            <div style={{ marginTop: 15 }}>
+                <TestPlaceInput setLocation={handleSetLocation} />
                 <TestMap location={location} />
-
             </div>
         </>
     );
--- revents/src/features/sandbox/testReducer.js
+++ revents/src/features/sandbox/testReducer.js
@@ -1,19 +1,36 @@
+import {asyncActionStart, asyncActionFinish, asyncActionError} from '../../app/async/asyncReducer';
+import { delay } from '../../app/common/util/util';
+//import {toast} from 'react-toastify';
+
 const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
 const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
 
 export function increment(amount) {
-    return {
-        type: INCREMENT_COUNTER,
-        payload: amount
+    return async function(dispatch) {
+      dispatch(asyncActionStart());
+      try {
+        await delay(1000);
+        dispatch({type: INCREMENT_COUNTER, payload: amount});
+        dispatch(asyncActionFinish());
+      } catch (error) {
+        dispatch(asyncActionError(error))
+      }
     }
 }
 
 export function decrement(amount) {
-    return {
-        type: DECREMENT_COUNTER,
-        payload: amount
+    return async function(dispatch) {
+      dispatch(asyncActionStart());
+      try {
+        await delay(1000);
+        dispatch({type: DECREMENT_COUNTER, payload: amount});
+        dispatch(asyncActionFinish());
+      } catch (error) {
+        dispatch(asyncActionError(error));
+       // toast.error(error);
+      }
     }
-}
+  }
 
 const initialState = {
     data: 42
@@ -29,7 +46,7 @@ export default function testReducer(state = initialState, {type, payload}) {
         case DECREMENT_COUNTER:
             return {
                 ...state,
-                data: state.data + payload
+                data: state.data - payload
             };
         default:
             return state
